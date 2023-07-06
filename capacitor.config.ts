61540d54f5d58923f16aa44dcb6af3b820ecc939
Gianluca Najera <gian.najera25@gmail.com>
2023-07-05 18:25:24 -0600

    Agregar proyecto con login y registro

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'GreenStamp',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
