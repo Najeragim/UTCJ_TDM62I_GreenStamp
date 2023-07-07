066a7c3acc20fe41c70dff6a0d344c3b4b61a75e
edgargdnz <edgar.godinez44@gmail.com>
2023-07-07 10:43:03 -0600

    se agrego funcion de botones

--- src/app/alumno/clases/clases.page.ts
+++ src/app/alumno/clases/clases.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-clases',
   templateUrl: './clases.page.html',
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClasesPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router){}
 
   ngOnInit() {
+  }
+
+  goToLogin(){
+    this.router.navigate(['/login'])
   }
 
 }
--- src/app/tutor/materias/materias.page.ts
+++ src/app/tutor/materias/materias.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-materias',
   templateUrl: './materias.page.html',
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MateriasPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router){}
 
   ngOnInit() {
+  }
+
+  goToLogin(){
+    this.router.navigate(['/login'])
   }
 
 }
